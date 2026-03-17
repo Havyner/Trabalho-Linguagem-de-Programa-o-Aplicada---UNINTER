7180d642a51000540401766806b723b0fd19ee33
Havyner <havy012_@hotmail.com>
2026-03-16 23:12:38 -0300

    texto menu

--- code/const.py
+++ code/const.py
@@ -1,5 +1,15 @@
 # Constantes
 
-#Tamanho da tela
+# C
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREEN = (17, 173, 38)
+
+# M
+MENU_OPTION = ('JOGAR',
+               'SCORE',
+               'SAIR')
+
+# W
 WIN_WIDTH = 600
 WIN_HEIGHT = 450
--- code/menu.py
+++ code/menu.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from pygame import Surface, Rect
 from pygame.font import Font
+from code.const import WIN_WIDTH, COLOR_WHITE, COLOR_BLACK, COLOR_GREEN, MENU_OPTION
 
 class Menu:
     def __init__(self, window):
@@ -17,6 +18,14 @@ class Menu:
 
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            # Texto do menu
+            self.menu_text(20, text="Havyner Siqueira RU: 5079616", text_color=COLOR_GREEN, text_center_pos=((WIN_WIDTH / 2), 20))
+            self.menu_text(70, text="KILL ZUMBIES", text_color=COLOR_BLACK, text_center_pos=((WIN_WIDTH / 2), 80))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(40, text=MENU_OPTION[i], text_color=COLOR_WHITE, text_center_pos=((WIN_WIDTH / 2), 230 + 50 * i))
+
+
             pg.display.flip()
 
             # Mantendo a janela aberta
@@ -28,8 +37,8 @@ class Menu:
     
     # Criando o texto
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pg.font.SysFont(name="Comic Sans", size=text_size)
-        text_surf: Surface = text_font.render(text, antialias:True, text_color).convert_alpha()
+        text_font: Font = pg.font.SysFont(name="Arial Black", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
         
